94700da63e0b83bda2a51da99119b36319b822ee
IMI-Designers <86943813+IMI-Designers@users.noreply.github.com>
2021-08-03 11:21:02 +0530

    upo

--- wp-content/themes/twentytwenty0/footer.php
+++ wp-content/themes/twentytwenty0/footer.php
@@ -76,11 +76,14 @@
     &copy; Copyright <strong><span>Anyar</span></strong>. All Rights Reserved
   </div>
   <div class="credits">
+    <!-- All the links in the footer should remain intact. -->
+    <!-- You can delete the links only if you purchased the pro version. -->
+    <!-- Licensing information: https://bootstrapmade.com/license/ -->
+    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/anyar-free-multipurpose-one-page-bootstrap-theme/ -->
     Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
   </div>
 </div>
-</footer>
-<!-- End Footer -->
+</footer><!-- End Footer -->
 
 <div id="preloader"></div>
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
@@ -96,27 +99,6 @@
 <!-- Template Main JS File -->
 <script src="assets/js/main.js"></script>
 
-<!-- Swiper JS -->
-<script src='https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/js/swiper.js'></script>
-
-<!-- Initialize Swiper -->
-<script>
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        loop: true,
-        autoplay: true,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-    });
-
-</script>
-
 </body>
+
 </html>
