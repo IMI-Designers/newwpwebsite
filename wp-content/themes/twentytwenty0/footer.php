6446931b420f482189b151343709a34c821015fc
IMI-Designers <86943813+IMI-Designers@users.noreply.github.com>
2021-08-03 11:04:34 +0530

    for

--- wp-content/themes/twentytwenty0/footer.php
+++ wp-content/themes/twentytwenty0/footer.php
@@ -86,17 +86,15 @@
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 <!-- Vendor JS Files -->
-<script src="<?php bloginfo('template_url'); ?>/assets/vendor/aos/aos.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/assets/vendor/glightbox/js/glightbox.min.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/assets/vendor/php-email-form/validate.js"></script>
-<script src="<?php bloginfo('template_url'); ?>/assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="/assets/vendor/aos/aos.js"></script>
+<script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="/assets/vendor/php-email-form/validate.js"></script>
+<script src="/assets/vendor/swiper/swiper-bundle.min.js"></script>
 
 <!-- Template Main JS File -->
-<script src="<?php bloginfo('template_url'); ?>/assets/js/main.js"></script>
-
-
+<script src="/assets/js/main.js"></script>
 
 <!-- Swiper JS -->
 <script src='https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.3/js/swiper.js'></script>
